5d5649407955977671d1c6257b77ace99372be19
woojin126 <woojin126@naver.com>
2022-03-21 22:59:08 +0900

    프로그래머스 신고하기 복습 완료

--- src/com/company/codingTest/Singo.java
+++ src/com/company/codingTest/Singo.java
@@ -10,31 +10,32 @@ public class Singo {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
 
-        Map<String, HashSet<String>> reports = new HashMap<>();
-        Map<String, Integer> mail_count = new HashMap<>();
+        Map<String,Integer> mail_list = new HashMap<>();
+        Map<String,HashSet<String>> people_list=  new HashMap<>();
 
-        //초기 셋팅
-        for (int i = 0; i < answer.length; i++) {
-            mail_count.put(id_list[i], i);
-            reports.put(id_list[i], new HashSet<>());
+        for (int i = 0 ; i < id_list.length ; i ++) {
+            mail_list.put(id_list[i] , i);
+            people_list.put(id_list[i] , new HashSet<>());
         }
 
-        //신고 한사람 신고 당한사람
         for (int i = 0; i < report.length; i++) {
-            String[] data = report[i].split(" ");
-            String from = data[0]; //신고한 사람
-            String to = data[1]; //신고 당한 사람
-            reports.get(to).add(from);
+            String[] people = report[i].split(" ");
+            String good = people[0];
+            String bad = people[1];
+
+            people_list.get(bad).add(good);
         }
 
-        for (int i = 0; i < id_list.length; i++) {
+        for (int i = 0 ; i < id_list.length ; i ++) {
+            HashSet<String> good = people_list.get(id_list[i]);
 
-            HashSet<String> froms = reports.get(id_list[i]);
-            if (froms.size() >= k) {
-                for (String s : froms)
-                    answer[mail_count.get(s)]++;
+            if (good.size() >= k ) {
+                for (String s : good) {
+                    answer[mail_list.get(s)] ++;
+                }
             }
         }
+
 
         return answer;
     }
