c0679f3df66757572b69aee2582af4e79bcf9ed7
woojin126 <woojin126@naver.com>
2022-03-30 18:28:58 +0900

    프로그래머스 체육복 문제.

--- src/com/company/codingTest/HealthCloth.java
+++ src/com/company/codingTest/HealthCloth.java
@@ -4,30 +4,26 @@ import java.util.HashSet;
 public class HealthCloth {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n;
+        HashSet<Integer> ko = new HashSet<>();
 
-        HashSet<Integer> l = new HashSet<>();
-        HashSet<Integer> r = new HashSet<>();
+        for (int r : reserve)
+            ko.add(r);
 
-        for(int ra : reserve)
-            r.add(ra);
-
-        for(int la : lost)
-            l.add(la);
-
-        for(int la : lost) {
-            if(r.contains(la-1))
-                r.remove(la-1);
-            else if(r.contains(la+1))
-                r.remove(la+1);
-            else answer--;
+        for (int i=0 ; i< lost.length ;i++) {
+            if (ko.contains(lost[i]))
+                ko.remove(lost[i]);
+            else if(ko.contains(lost[i]-1))
+                ko.remove(lost[i]-1);
+            else if (ko.contains(lost[i]+1))
+                ko.remove(lost[i]+1);
+            else answer --;
         }
-
         return answer;
     }
 
     public static void main(String[] args) {
         HealthCloth healthCloth = new HealthCloth();
-        int solution = healthCloth.solution(5, new int[]{3}, new int[]{1});
+        int solution = healthCloth.solution(5, new int[]{2,4}, new int[]{1,3,5});
         System.out.println(solution);
     }
 }
