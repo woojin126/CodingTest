5bd04d8cfdc129a0a1ffcf9af5509035b4b7f008
woojin126 <woojin126@naver.com>
2022-03-31 13:26:31 +0900

    프로그래머스 체육복

--- src/com/company/codingTest/HealthCloth.java
+++ src/com/company/codingTest/HealthCloth.java
@@ -1,29 +1,42 @@
 package com.company.codingTest;
+
+
 import java.util.HashSet;
 
 public class HealthCloth {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n;
-        HashSet<Integer> ko = new HashSet<>();
+        int answer = n - lost.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(int r : reserve)
+            set.add(r);
 
-        for (int r : reserve)
-            ko.add(r);
-
-        for (int i=0 ; i< lost.length ;i++) {
-            if (ko.contains(lost[i]))
-                ko.remove(lost[i]);
-            else if(ko.contains(lost[i]-1))
-                ko.remove(lost[i]-1);
-            else if (ko.contains(lost[i]+1))
-                ko.remove(lost[i]+1);
-            else answer --;
+        for(int i = 0 ; i < lost.length ; i++) {
+            if(set.contains(lost[i])) {
+                set.remove(lost[i]);
+                answer++;
+                lost[i] = -1;
+            }
         }
+
+        for (int i =0 ; i < lost.length ; i++) {
+            if (set.contains(lost[i] -1)) {
+                answer++;
+                set.remove(lost[i] -1);
+            }else if (set.contains(lost[i] +1)) {
+                answer++;
+                set.remove(lost[i] +1);
+            }
+        }
+
+
+
+
         return answer;
     }
 
     public static void main(String[] args) {
         HealthCloth healthCloth = new HealthCloth();
-        int solution = healthCloth.solution(5, new int[]{2,4}, new int[]{1,3,5});
+        int solution = healthCloth.solution(3, new int[]{3}, new int[]{1});
         System.out.println(solution);
     }
 }
