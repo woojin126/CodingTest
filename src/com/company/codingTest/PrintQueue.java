88ddb937e84db240fe9c68f2ae9332c0525ede69
woojin126 <woojin126@naver.com>
2022-11-24 13:33:51 +0900

    백준 프린터 큐

--- src/com/company/codingTest/PrintQueue.java
+++ src/com/company/codingTest/PrintQueue.java
@@ -2,48 +2,42 @@ package com.company.codingTest;
 
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class PrintQueue {
-
-    private static void setting(Scanner sc, int N, LinkedList<int[]> q) {
-        for (int j = 0; j < N; j++) {
-            q.add(new int[]{j, sc.nextInt()});
-        }
-    }
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
 
-        for (int i=0 ; i<T ; i++) {
+        for (int i = 0; i < T; i++) {
             int N = sc.nextInt();
             int M = sc.nextInt();
-            LinkedList<int[]> q = new LinkedList<>();
-            setting(sc, N, q);
+            int cnt = 0;
+            Queue<int[]> queue = new LinkedList<>();
+            for (int j = 0; j < N; j++) {
+                queue.add(new int[] {j,sc.nextInt()});
+            }
 
-            int count = 0;
             while (true) {
-                boolean isMax = true;
-                int[] firstValue = q.poll();
+                int now[] = queue.remove();
+                boolean flag = true;
 
-                for (int k = 0 ; k < q.size(); k++) {
-                    if (firstValue[1] < q.get(k)[1]) {
-                        q.offer(firstValue);
-                        isMax = false;
+                for (int q[] : queue) {
+                    if(q[1] > now[1]) {
+                        flag = false;
+                        break;
                     }
                 }
 
-                if (!isMax) continue;
-
-                count++;
-                if (M == firstValue[0])
-                    break;
+                if(flag) {
+                    cnt++;
+                    if(now[0] == M) break;
+                }else {
+                    queue.add(now);
+                }
             }
-
-            System.out.println(count);
+            System.out.println(cnt);
         }
     }
-
 }
-
