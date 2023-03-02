ebea9f99713fd4d25ca8bf06da410349c79644ad
woojin126 <woojin126@naver.com>
2023-03-02 22:22:15 +0900

    중복문자제거문제. ArrayList를 사용하여 해결

--- README.md
+++ README.md
@@ -262,3 +262,59 @@ public class Main {
 
 
 ```
+
+
+## 6. 중복문자제거
+   설명
+
+소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램을 작성하세요.
+
+중복이 제거된 문자열의 각 문자는 원래 문자열의 순서를 유지합니다.
+
+
+입력
+첫 줄에 문자열이 입력됩니다. 문자열의 길이는 100을 넘지 않는다.
+
+
+출력
+첫 줄에 중복문자가 제거된 문자열을 출력합니다.
+
+
+예시 입력 1
+
+ksekkset
+예시 출력 1
+
+kset
+```
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static String removeDuplicate(String name) {
+        ArrayList<Character> list = new ArrayList<>();
+        StringBuilder answer = new StringBuilder();
+        char[] s = name.toCharArray();
+        for (int i = 0; i < name.length(); i++) {
+            if (!list.contains(s[i])) {
+                answer.append(s[i]);
+            }
+            list.add(s[i]);
+        }
+
+        return answer.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        String s = removeDuplicate(text);
+        System.out.println(s);
+    }
+}
+
+
+```
--- src/com/company/codingTest/Main.java
+++ src/com/company/codingTest/Main.java
@@ -1,32 +1,30 @@
 package com.company.codingTest;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-    public static String reverseString(String name) {
-        String answer = "";
-        int firstIndex = 0;
-        int lastIndex = name.length()-1;
+    public static String removeDuplicate(String name) {
+        ArrayList<Character> list = new ArrayList<>();
+        StringBuilder answer = new StringBuilder();
         char[] s = name.toCharArray();
-        while (firstIndex < lastIndex) {
-            if (!((s[firstIndex] >= 'A' && s[firstIndex] <= 'Z') || (s[firstIndex] >= 'a' && s[firstIndex] <= 'z'))) firstIndex++;
-            else if (!((s[lastIndex] >= 'A' && s[lastIndex] <= 'Z') || (s[lastIndex] >= 'a' && s[lastIndex] <= 'z'))) lastIndex--;
-            else {
-                char tmp = s[firstIndex];
-                s[firstIndex] = s[lastIndex];
-                s[lastIndex] = tmp;
-                firstIndex++;
-                lastIndex--;
+        for (int i = 0; i < name.length(); i++) {
+            if (!list.contains(s[i])) {
+                answer.append(s[i]);
             }
-
-            answer =  String.valueOf(s);
+            list.add(s[i]);
         }
-        return answer;
+
+        return answer.toString();
     }
+
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         String text = sc.next();
-        System.out.println(reverseString(text));
+        String s = removeDuplicate(text);
+        System.out.println(s);
     }
 }
