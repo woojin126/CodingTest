0bb82d068a7024bf178709169a731995b39f368d
woojin126 <woojin126@naver.com>
2023-03-07 21:02:47 +0900

    암호 해석

--- src/com/company/codingTest/Main.java
+++ src/com/company/codingTest/Main.java
@@ -2,29 +2,32 @@ package com.company.codingTest;
 import java.util.*;
 
 class Main {
-    public static String solution(String s) {
-        StringBuilder answer = new StringBuilder();
-        int count = 1;
-        s = s + " ";
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                count++;
-            } else if (count == 1) {
-                answer.append(s.charAt(i));
-            } else {
-                answer.append(s.charAt(i));
-                answer.append(count);
-                count = 1;
+        public static String solution(String s, int n) {
+            String answer = "";
+            for (int i = 0 ; i < n ; i++) {
+                String value = s.substring(0, 7).replace('#', '1').replace('*', '0');
+
+                int count = 0;
+                int sum = 0;
+                //십진수 변환
+                for (int j=value.length()-1; j >= 0 ;j--) {
+                    sum += Integer.parseInt(String.valueOf(value.charAt(j))) * Math.pow(2, count);
+                    count++;
+                }
+
+                answer += (char)sum;
+                s = s.substring(7);
+
             }
+            return answer;
         }
 
-        return answer.toString();
-    }
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
+        int a = kb.nextInt();
         String s = kb.next();
-        System.out.println(solution(s));
+        System.out.println(solution(s,a));
     }
 }
 
