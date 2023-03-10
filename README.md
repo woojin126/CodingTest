2f8d8cb92447696485b6d8d4c8632a59a0e802fb
woojin <woojin126@naver.com>
2023-03-10 21:46:02 +0900

    가위 바위 보 다른풀이 및 다른 입출력

--- README.md
+++ README.md
@@ -963,6 +963,39 @@ class Main {
         System.out.println(solution(A,B));
 
 
+    
+}
+```
+
+``` bufferedReader 사용 및 다른풀이법
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main {
+    public static String solution(int a, int b) {
+        if (a==b) return "D";
+
+        if (a==1 && b==3) return "A";
+        else if (a==2 && b==1) return "A";
+        else if (a==3 && b==2) return "A";
+        else return "B";
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer A = new StringTokenizer(br.readLine());
+        StringTokenizer B = new StringTokenizer(br.readLine());
+
+        while (N != 0) {
+            N--;
+            System.out.println(solution(Integer.parseInt(A.nextToken()),Integer.parseInt(B.nextToken())));
+        }
+
     }
 }
+
+
 ```
--- src/com/company/codingTest/Main.java
+++ src/com/company/codingTest/Main.java
@@ -1,35 +1,28 @@
 package com.company.codingTest;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 class Main {
-    public static String solution(int[] a, int[] b) {
-        String answer = "";
-        for (int i = 0 ; i < a.length ;i++) {
-            if (a[i] == b[i]) answer += "D";
-            else if (a[i] == 1 && b[i] == 3) answer += "A";
-            else if (a[i] == 2 && b[i] == 1) answer += "A";
-            else if (a[i] == 3 && b[i] == 2) answer += "A";
-            else answer += "B";
-        }
-        return answer;
+    public static String solution(int a, int b) {
+        if (a==b) return "D";
+
+        if (a==1 && b==3) return "A";
+        else if (a==2 && b==1) return "A";
+        else if (a==3 && b==2) return "A";
+        else return "B";
     }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer A = new StringTokenizer(br.readLine());
+        StringTokenizer B = new StringTokenizer(br.readLine());
 
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = new int[N];
-        int[] B = new int[N];
-
-        //꼮 이렇게 초기값 셋팅을 해야하는지? 생각이 더필요
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+        while (N != 0) {
+            N--;
+            System.out.println(solution(Integer.parseInt(A.nextToken()),Integer.parseInt(B.nextToken())));
         }
-
-        for (int i = 0; i < N; i++) {
-            B[i] = sc.nextInt();
-        }
-        System.out.println(solution(A,B));
-
 
     }
 }
