4ac399fddce938470e00acbce5cbf76014197ec2
woojin126 <woojin126@naver.com>
2023-03-14 23:51:17 +0900

    피보나치수열 풀이

--- src/com/company/codingTest/Main.java
+++ src/com/company/codingTest/Main.java
@@ -3,27 +3,26 @@ package com.company.codingTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main {
-    public static String solution(int a, int b) {
-        if (a==b) return "D";
+    public static int[] solution(int N) {
+        int[] arr = new int[N];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2 ; i < N ; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
+        }
 
-        if (a==1 && b==3) return "A";
-        else if (a==2 && b==1) return "A";
-        else if (a==3 && b==2) return "A";
-        else return "B";
+        return arr;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer A = new StringTokenizer(br.readLine());
-        StringTokenizer B = new StringTokenizer(br.readLine());
 
-        while (N != 0) {
-            N--;
-            System.out.println(solution(Integer.parseInt(A.nextToken()),Integer.parseInt(B.nextToken())));
+        int[] solution = solution(N);
+
+        for (int i : solution) {
+            System.out.print(i + " ");
         }
-
     }
 }
 
