554ede2aefea973cacb84ed9bcad88ee01caeec9
woojin126 <woojin126@naver.com>
2023-03-16 22:05:14 +0900

    에라토스테네스의체

--- src/com/company/codingTest/Main.java
+++ src/com/company/codingTest/Main.java
@@ -3,26 +3,28 @@ package com.company.codingTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-class Main {
-    public static int[] solution(int N) {
-        int[] arr = new int[N];
-        arr[0] = 1;
-        arr[1] = 1;
-        for (int i = 2 ; i < N ; i++) {
-            arr[i] = arr[i-2] + arr[i-1];
-        }
+import java.util.Arrays;
 
-        return arr;
+class Main {
+    public static int solution(int N) {
+        Boolean[] tf = new Boolean[N+1];;
+        tf[0] = true;
+        tf[1] = true;
+        for (int i = 2; i < N ; i++) {
+            if (tf[i]) {
+                for (int j=i; j<=N; j=j+i) tf[j]=false;
+            }
+        }
+        int answer = 0;
+        for (int i = 0 ; i < N ; i ++) {
+            if (tf[i]) answer++;
+        }
+        return answer;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        int[] solution = solution(N);
-
-        for (int i : solution) {
-            System.out.print(i + " ");
-        }
+        System.out.println(solution(N));
     }
 }
 
