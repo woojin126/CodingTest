4dfac3ec23081b156a7feeae7a53386446a327b8
woojin <woojin126@naver.com>
2023-03-18 23:53:11 +0900

    소수(에라토스테네스 체)

--- src/com/company/codingTest/Main.java
+++ src/com/company/codingTest/Main.java
@@ -7,19 +7,20 @@ import java.util.Arrays;
 
 class Main {
     public static int solution(int N) {
-        Boolean[] tf = new Boolean[N+1];;
-        tf[0] = true;
-        tf[1] = true;
-        for (int i = 2; i < N ; i++) {
-            if (tf[i]) {
-                for (int j=i; j<=N; j=j+i) tf[j]=false;
+        int[] arr = new int[N+1];
+
+        for (int i = 2 ; i <= N ; i++) {
+            arr[i] = i;
+        }
+        for (int i = 2 ; i <= N ; i++) {
+            if (arr[i] == 0) continue;
+
+            for (int j = i*2 ; j <= N ; j+=i) {
+                arr[j] = 0;
             }
         }
-        int answer = 0;
-        for (int i = 0 ; i < N ; i ++) {
-            if (tf[i]) answer++;
-        }
-        return answer;
+
+        return (int) Arrays.stream(arr).filter(k -> k != 0).count();
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
