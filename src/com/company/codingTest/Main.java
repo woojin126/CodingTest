24d2d7fee039e65eb91dc6687d26bcab439de553
woojin126 <woojin126@naver.com>
2023-03-21 15:41:28 +0900

    점수계산문제 readMe 풀이 확인

--- src/com/company/codingTest/Main.java
+++ src/com/company/codingTest/Main.java
@@ -3,52 +3,29 @@ package com.company.codingTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
+import java.util.Arrays;
 class Main {
-    public static ArrayList<Integer> solution(int N) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        //입력값 역순
-        int res = reverseOrder(N);
-        //소수판별
-        if (isPrime(res)) answer.add(res);
-        return answer;
-
-    }
-
-    private static int reverseOrder(int N) {
-        int nm;
-        int res = 0;
-        while (N != 0) {
-            nm = N % 10;
-            res = res * 10 + nm;
-            N = N / 10;
+    public static int solution(int[] N) {
+        int value = 0;
+        int sum = 0;
+        for (int i = 0; i < N.length ; i++) {
+            if (N[i] == 0) {
+                sum = 0;
+            } else {
+                value += ++sum;
+            }
         }
-        return res;
-    }
-
-    public static boolean isPrime(int p) {
-        if (p == 1) return false;
-        for (int i = 2; i <= (int) Math.sqrt(p); i++) {
-            if (p % i == 0) return false;
-        }
-
-        return true;
+        return value;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        st = new StringTokenizer(s);
-        while (st.hasMoreTokens()) {
-            ArrayList<Integer> list = solution(Integer.parseInt(st.nextToken()));
-            for (Integer result : list) {
-                System.out.print(result + " ");
-            }
-        }
+        String[] input = br.readLine().split(" ");
+
+        int[] value = Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(solution(value));
 
     }
 }
