7cedadacb51dee0a993621eaec0f48a1cb206040
woojin126 <woojin126@naver.com>
2023-03-23 18:07:07 +0900

    봉우리 문제

--- README.md
+++ README.md
@@ -1463,3 +1463,77 @@ class Main {
 
 
 ```
+
+## 10. 봉우리
+    설명
+
+지도 정보가 N*N 격자판에 주어집니다. 각 격자에는 그 지역의 높이가 쓰여있습니다.
+
+각 격자판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다. 봉우리 지역이 몇 개 있는 지 알아내는 프로그램을 작성하세요.
+
+격자의 가장자리는 0으로 초기화 되었다고 가정한다.
+
+만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.
+
+Image1.jpg
+
+
+입력
+첫 줄에 자연수 N이 주어진다.(2<=N<=50)
+
+두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는다.
+
+
+출력
+봉우리의 개수를 출력하세요.
+
+
+예시 입력 1
+
+5
+5 3 7 2 3
+3 7 1 6 1
+7 2 5 3 4
+4 3 6 4 1
+8 7 3 5 2
+예시 출력 1
+
+10
+
+``` 
+봉우리 첫풀이
+각 인덱스에서 상하좌우를 비교하는데있어서 (0,0) 같은경우 (-1,0) (0,-1) 와 비교하게되면 boundException이 발생하기때문에,
+입력받은 값을 제외하고 배열의 주면은 0으로 채워넣어서 풀이. 
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static int solution(int[][] arr) {
+        int count = 0;
+        for (int i = 1; i < arr.length - 1; i++) {
+            for (int j = 1; j < arr.length - 1; j++) {
+                if (arr[i][j] > arr[i + 1][j] && arr[i][j] > arr[i][j + 1] && arr[i][j] > arr[i - 1][j] && arr[i][j] > arr[i][j - 1])
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] arr = new int[N + 2][N + 2];
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; j < N + 1; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int solution = solution(arr);
+        System.out.println(solution);
+
+    }
+}
+
+```
--- src/com/company/codingTest/Main.java
+++ src/com/company/codingTest/Main.java
@@ -5,40 +5,29 @@ import java.util.Scanner;
 
 class Main {
     public static int solution(int[][] arr) {
-
-        int max = 0;
-        int sum, sum2;
-        for (int i = 0 ; i < arr[0].length ; i++) {
-            sum=sum2=0;
-            for (int j = 0 ; j < arr.length ; j++) {
-                sum += arr[i][j];
-                sum2 += arr[j][i];
+        int count = 0;
+        for (int i = 1; i < arr.length - 1; i++) {
+            for (int j = 1; j < arr.length - 1; j++) {
+                if (arr[i][j] > arr[i + 1][j] && arr[i][j] > arr[i][j + 1] && arr[i][j] > arr[i - 1][j] && arr[i][j] > arr[i][j - 1])
+                    count++;
             }
-
-            int bi = Math.max(sum, sum2);
-            if (bi > max) max = bi;
         }
-
-        sum=sum2=0;
-        for (int i = 0; i < arr[0].length ;i++) {
-            sum += arr[i][i];
-            sum2 += arr[i][i];
-        }
-        int answer = Math.max(max, Math.max(sum, sum2));
-        return answer;
+        return count;
     }
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[][] arr = new int[N][N];
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<N ;j++) {
+        int[][] arr = new int[N + 2][N + 2];
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; j < N + 1; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        System.out.println(solution(arr));
+        int solution = solution(arr);
+        System.out.println(solution);
+
     }
 }
 
